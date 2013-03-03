fb85953bb7405d7c9f4157a123178bb242e59ef4
Dominic Tarr <dominic.tarr@gmail.com>
2013-03-03 21:03:27 +1300

    remove test memory db stuff

--- index.js
+++ index.js
@@ -1,94 +1,27 @@
 
 var EventEmitter = require('events').EventEmitter
-var next = process.nextTick
-var SubDb = require('./sub')
+var next         = process.nextTick
+var SubDb        = require('./sub')
 
-module.exports = function () {
+var Hooks   = require('level-hooks')
 
-  var store = {}
-  var emitter = new EventEmitter()
+module.exports   = function (db, sep) {
+  //use \xff (255) as the seperator,
+  //so that sections of the database will sort after the regular keys
+  sep = sep || '\xff'
+  Hooks(db)
 
-  emitter.namespace = function (prefix) {
-    return new SubDb(emitter, prefix)
+  db.namespace = function (prefix) {
+    return new SubDb(db, prefix, sep)
   }
 
-  emitter.put = function (key, value, opts, cb) {
-    if(!cb) cb = opts, opts = null
-    emitter._change(
-      [{type: 'put', key: key, value: value}]
-    , cb)
-  }
-
-  emitter.del = function (key, value, opts, cb) {
-    if(!cb) cb = opts, opts = null
-    emitter._change(
-      [{type: 'del', key: key, value: value}]
-    , cb)
-  }
-
-  emitter._change = function (ch, cb) {
-    console.log(ch)
-    ch = emitter._hook(ch)
-    next(function () {
-      ch.forEach(function (e) {
-        if(e.type == 'del') delete store[e.key]
-        else                store[e.key] = e.value
-      })
-      if(ch.length == 1)
-        emitter.emit(ch[0].type, ch[0].key, ch[0].value)
-      else
-        emitter.emit('batch', ch)
-      cb()
-    })
-  }
-
-  emitter.batch = function (changes, opts, cb) {
-    if(!cb) cb = opts, opts = null
-    emitter._change(changes, cb)
-    return emitter
-  }
-
-  emitter.store = store
-
-  emitter._hook = function (batch) {
-    if(!Array.isArray(batch))
-      batch = [batch]
-    var hooks = this._hooks || []
-    function hook(e, i) {
-      hooks.forEach(function (h) {
-        console.log('hook', e, h.prefix)
-          if(e.key.indexOf(h.prefix) == 0)
-            h.hook(e, function (ch, db) {
-              if(ch === false) {
-                return delete batch[i]
-              }
-              ch.key = (db ? db.prefix() : h.prefix) + ch.key
-              console.log('batch - add', batch, ch)
-              var j = batch.push(ch)
-              hook(ch, batch.length - 1)
-              
-            })
-      })
-    }
-
-    batch.forEach(hook)
-    return batch
-  }
-
-  emitter._pre = function (prefix, hook) {
-    this._hooks = this._hooks || []
-    this._hooks.push({prefix: prefix, hook: hook})
-    console.log("_HOOKS", this._hooks)
-  }
-
-  emitter.pre = function (hook) {
-    emitter._pre('', hook)
-  }
-
-  emitter.prefix = function () {
+  db.prefix = function () {
     return ''
   }
 
-  return emitter
+  db.pre = db.hooks.pre
+  db.post = db.hooks.post
+
+  return db
 }
 
