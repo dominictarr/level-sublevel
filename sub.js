837db067bd8313a42fd2b10faea4d987613978e7
Dominic Tarr <dominic.tarr@gmail.com>
2013-03-03 21:02:29 +1300

    round out subdb api

--- sub.js
+++ sub.js
@@ -1,7 +1,7 @@
 var Bucket = require('range-bucket')
 var Hooks  = require('level-hooks')
 
-function SubDB (db, prefix) {
+function SubDB (db, prefix, sep) {
   if(!(this instanceof SubDB)) return new SubDB(db, prefix)
   if(!db)     throw new Error('must provide db')
   if(!prefix) throw new Error('must provide prefix')
@@ -9,14 +9,28 @@ function SubDB (db, prefix) {
   Hooks(db)
 
   this._parent = db
-  this._prefix = prefix
   this._bucket = Bucket(prefix)
+  this._sep = sep || '\xff'
+  this._prefix = prefix
+  var self = this
+  this.hooks = {
+    pre: function () {
+      self.pre.apply(self, arguments)
+    },
+    post: function () {
+      self.pre.apply(self, arguments)
+    }
+  }
 }
 
 var SDB = SubDB.prototype
 
 SDB._key = function (key) {
-  return this._bucket(key[0] === '\xFF' ? key.substring(1) : key)
+//  return this._bucket(key[0] === this._sep ? key.substring(1) : key)
+  var s = this._prefix + this._sep + key
+  if(!this._parent._parent)
+    return this._sep + s
+  return s
 }
 
 SDB.namespace = function (prefix) {
@@ -24,7 +38,6 @@ SDB.namespace = function (prefix) {
 }
 
 SDB.put = function (key, value, opts, cb) {
-  console.log(key)
   //prehook
   this._parent.put(this._key(key), value, opts, cb)
 }
@@ -42,7 +55,37 @@ SDB.batch = function (changes, opts, cb) {
 }
 
 SDB.prefix = function () {
-  return this._parent.prefix() + '\xFF' + this._prefix + '\xFF'
+  return this._parent.prefix() + this._sep + this._prefix + this._sep
+}
+
+;['createReadStream', 'createKeyStream', 'createValueStream']
+  .forEach(function (createStream) {
+    SDB[createStream] = function () {
+      var r = root(this)
+      var p = this.prefix()
+      return r[createStream].apply(r, arguments)
+        .on('data', function (d) {
+          //mutate the prefix!
+          d.key = d.key.replace(prefix, '')
+        })
+    }
+  })
+
+SDB.writeStream = function () {
+  var r = root(this)
+  var p = this.prefix()
+  var ws = r.createWriteStream.apply(r, arguments)
+  var write = ws.write
+  ws.write = function (data) {
+    data.key = p + data.key
+    return write.call(ws, data)
+  }
+  return ws
+}
+
+SDB.approximateSize = function () {
+  var r = root(db)
+  return r.approximateSize.apply(r, arguments)
 }
 
 function root(db) {
@@ -52,58 +95,23 @@ function root(db) {
 
 SDB.pre = function (hook) {
   var r = root(this._parent)
-  r._pre(this.prefix(), hook)
+  r.hooks.pre({
+    start: this.prefix(),
+    end  : this.prefix() + this._sep
+  }
+  , hook)
+  return this
+}
+
+SDB.post = function (hook) {
+  var r = root(this._parent)
+  r.hooks.post({
+    start: this.prefix(),
+    end  : this.prefix() + this._sep
+  }
+  , hook)
+  return this
 }
 
 var exports = module.exports = SubDB
-
-/*
-exports.couple = function (a, b, transform) {
-  //Hooks(from); Hooks(to)
-
-  //first, find the mutual parent
-  if(a._parent == b._parent) { //<-- simple case
-    //register listener on the parent.
-    //get the prefixes between the 
-    var parent = a._parent
-    Hooks(a._parent)
-    a.pre(function (changes) {
-      changes.forEach(function (e) {
-        if(
-      })
-    })
-  }
-}
-*/
-/*
-//hmm, maybe the db needs some sort of pipe like thing?
-//stream from
-
-db.pipe(db2) //and check if the databases has a common ancestor,
-             //if so, insert atomically.
-//cos, the thing I need is for it to be easy to transform data from one db to another.
-//and... just have a function to transform
-
-so... maybe make 
-
-//compare the prefixes of db and jobs, so you know where to register the listener!
-db.pre(jobs, function (batch) {
-  return queueFrom(batch)
-})
-
-couple(db, jobs).on(function (v) {
-  return {key: k, value: v}
-  //and it will be prefixed with the right prefix for jobs.
-})
-
-jobs.post(function (item) {
-  //process the job...
-  asyncWhatever(function (err) {
-    if(err) return retry(item)
-    jobs.del(item.key)
-  })
-})
-
-jobs.trigger(function(
-*/
 
