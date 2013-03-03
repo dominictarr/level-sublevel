debdf2971b9198a88b5278ede70319d78b50700d
Dominic Tarr <dominic.tarr@gmail.com>
2013-03-03 21:00:53 +1300

    rewrite test

--- test/hook.js
+++ test/hook.js
@@ -1,31 +1,54 @@
 
-var Mem = require('../')
+require('tape')('sublevel', function (t) {
 
-var base = Mem()
-var a    = base.namespace('A')
-var b    = base.namespace('SEQ')
-var c    = base.namespace('___')
-var _a   = base.namespace('_A')
-var i = 0
+  require('rimraf').sync('/tmp/test-subdb')
 
-a.pre(function (ch, add) {
-  add({key: i++, value: ch.key}, b)
+  var base = require('levelup')('/tmp/test-sublevel', function () {
+    var Sublevel = require('../')
+
+    Sublevel(base, '~')
+
+    var a    = base.namespace('A')
+    var b    = base.namespace('SEQ')
+
+    var i = 0
+
+    function all(db, cb) {
+      var o = {}
+      db.readStream().on('data', function (data) {
+        o[data.key.toString()] = data.value.toString()
+      })
+      .on('end', function () {
+        cb(null, o)
+      })
+      .on('error', cb)
+    }
+
+    a.pre(function (ch, add) {
+      add({key: i++, value: ch.key, type: 'put'}, b)
+    })
+
+    var n = 3, _a, _b, _c
+
+    a.put('a', _a ='AAA_'+Math.random(), next)
+    a.put('b', _b = 'BBB_'+Math.random(), next)
+    a.put('c', _c = 'CCC_'+Math.random(), next)
+
+    function next () {
+      if(--n) return
+
+      all(base, function (err, obj) {
+        console.log(obj)
+        t.deepEqual(obj, 
+          { '~A~a': _a,
+            '~A~b': _b,
+            '~A~c': _c,
+            '~SEQ~0': '~A~a',
+            '~SEQ~1': '~A~b',
+            '~SEQ~2': '~A~c' })
+        t.end()
+      })
+    }
+  })
+
 })
-
-b.pre(function (ch, add) {
-  add({key: i++, value: ch.key}, c)
-})
-
-a.put('a', Math.random(), function () {
-  console.log(base.store)
-})
-
-a.put('b', Math.random(), function () {
-  console.log(base.store)
-})
-
-a.put('c', Math.random(), function () {
-  console.log(base.store)
-})
-
-
