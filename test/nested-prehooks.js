a421dd8e1615aeec6fa3f46c3cbb0b60379072c1
Dominic Tarr <dominic.tarr@gmail.com>
2013-03-09 17:20:46 +1300

    test for prehooks on nested sections

---
+++ test/nested-prehooks.js
@@ -0,0 +1,53 @@
+
+require('tape')('sublevel', function (t) {
+
+  require('rimraf').sync('/tmp/test-sublevel')
+
+  var base = require('levelup')('/tmp/test-sublevel', function () {
+    var Sublevel = require('../')
+
+    Sublevel(base, '~')
+
+    var a    = base.sublevel('A')
+    var a_a    = a.sublevel('A')
+
+    var as = {}
+    var aas = {}
+
+    a.pre(function (e) {
+      as[e.key] = e.value
+      console.log('A   :', e)
+    })
+
+    a_a.pre(function (e) {
+      aas[e.key] = e.value
+      console.log('A_A :', e)
+    })
+
+    var n = 3
+    a.put('apple', '1', next)
+    a.put('banana', '2', next)
+
+    a_a.put('aardvark', 'animal1', next)
+
+    function next() {
+      if(--n) return
+      t.deepEqual(as, {apple: '1', banana: '2'})
+      t.deepEqual(aas, {aardvark: 'animal1'})
+      t.end()
+    }
+
+  return
+
+    function all(db, cb) {
+      var o = {}
+      db.createReadStream().on('data', function (data) {
+        o[data.key.toString()] = data.value.toString()
+      })
+      .on('end', function () {
+        cb(null, o)
+      })
+      .on('error', cb)
+    }
+  })
+})
