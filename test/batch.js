4446d7c8725b6abbf5584aa035dc15b8596aa50f
Dominic Tarr <dominic.tarr@gmail.com>
2013-03-09 17:22:38 +1300

    update test for batches on subsection

--- test/batch.js
+++ test/batch.js
@@ -77,4 +77,44 @@ test('sublevel - prefixed batches', function (t) {
 
 })
 
+test('sublevel - prefixed batches on subsection', function (t) {
+
+  require('rimraf').sync('/tmp/test-sublevel3')
+  var base = require('levelup')('/tmp/test-sublevel3')
+
+  var Sublevel = require('../')
+
+  Sublevel(base, '~')
+
+  var a    = base.sublevel('A')
+  var b    = base.sublevel('B')
+
+  var obj = {}
+
+  a.batch([
+    {key: 'a', value: 1, type: 'put', prefix: base},
+    {key: 'b', value: 2, type: 'put', prefix: b},
+    {key: 'c', value: 3, type: 'put', prefix: base},
+    {key: 'd', value: 4, type: 'put'},
+    {key: 'e', value: 5, type: 'put', prefix: base},
+  ], function (err) {
+    base.createReadStream()
+      .on('data', function (ch) {
+        obj[ch.key] = ch.value
+      })
+      .on('end', function () {
+        t.deepEqual(obj, {
+          'a': '1',
+          'c': '3',
+          'e': '5',
+          '~A~d': '4',
+          '~B~b': '2'
+        })
+        console.log(obj)
+        t.end()
+      })
+  })
+
+})
+
 
