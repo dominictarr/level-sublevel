467dbf727799cc37972a247aa08a9de052c4a20f
Dominic Tarr <dominic.tarr@gmail.com>
2013-04-16 23:31:09 +0100

    fix test

--- test/prehook-insert.js
+++ test/prehook-insert.js
@@ -1,6 +1,6 @@
 var Sublevel = require('../')
 
-require('tape')('sublevel', function (t) {
+require('tape')('insert in prehook', function (t) {
 
   require('rimraf').sync('/tmp/test-sublevel')
 
@@ -19,8 +19,8 @@ require('tape')('sublevel', function (t) {
     console.log('A   :', op)
     add({
       key: op.key, value: op.value, 
-      type: 'put', prefix: b//.prefix()
-    }, b)
+      type: 'put', prefix: b.prefix()
+    })
   })
 
   var val = 'random_' + Math.random()
@@ -31,4 +31,41 @@ require('tape')('sublevel', function (t) {
       t.end()
     })
   })
+
 })
+
+require('tape')('insert in prehook 2', function (t) {
+
+  require('rimraf').sync('/tmp/test-sublevel2')
+
+  var base = Sublevel(require('levelup')('/tmp/test-sublevel2'))
+
+  Sublevel(base, '~')
+
+  var a   = base.sublevel('A')
+  var b   = base.sublevel('B')
+
+  var as = {}
+  var aas = {}
+
+  a.pre(function (op, add) {
+    as[op.key] = op.value
+    console.log('A   :', op)
+    add({
+      key: op.key, value: op.value, 
+      type: 'put', prefix: b
+    })
+  })
+
+  var val = 'random_' + Math.random()
+  a.put('foo', val, function () {
+
+    b.get('foo', function (err, _val) {
+      t.equal(_val, val)
+      t.end()
+    })
+  })
+
+})
+
+
