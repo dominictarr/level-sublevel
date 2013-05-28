154df878e4a0ffaa9ea757f062c66dfb80c89e08
Dominic Tarr <dominic.tarr@gmail.com>
2013-05-29 01:13:01 +0200

    test that can batch from the bottom db

--- test/batch.js
+++ test/batch.js
@@ -1,9 +1,26 @@
 var test = require('tape')
 
+function all (db, cb) {
+  var obj = {}, fin = false
+
+  function done (err) {
+    if(fin) return
+    fin = true
+    cb(err, obj)
+  }
+  
+  db.createReadStream({end: '\xff\xff'})
+    .on('data', function (ch) {
+      obj[ch.key] = ch.value
+    })
+    .on('end', done)
+    .on('error', done)
+}
+
 test('sublevel - batch', function (t) {
 
   require('rimraf').sync('/tmp/test-sublevel')
-  var base = require('levelup')('/tmp/test-sublevel')
+  var base = require('level')('/tmp/test-sublevel')
 
   var Sublevel = require('../')
 
@@ -13,7 +30,6 @@ test('sublevel - batch', function (t) {
   var b    = base.sublevel('B')
 
 
-  var keys = ''
   var sum  = 0
 
   a.batch([
@@ -23,16 +39,16 @@ test('sublevel - batch', function (t) {
     {key: 'd', value: 4, type: 'put'},
     {key: 'e', value: 5, type: 'put'},
   ], function (err) {
-    a.createReadStream()
-      .on('data', function (ch) {
-        keys += ch.key
-        sum += Number(ch.value)
-      })
-      .on('end', function () {
-        t.equal(keys, 'abcde')
-        t.equal(sum, 15)
-        t.end()
-      })
+    all(a, function (err, obj) {
+      t.notOk(err)
+      var keys = Object.keys(obj).join('')
+      for(var k in obj) {
+        sum += Number(obj[k])
+      }
+      t.equal(keys, 'abcde')
+      t.equal(sum, 15)
+      t.end()
+    })
   })
 
 })
@@ -40,7 +56,7 @@ test('sublevel - batch', function (t) {
 test('sublevel - prefixed batches', function (t) {
 
   require('rimraf').sync('/tmp/test-sublevel2')
-  var base = require('levelup')('/tmp/test-sublevel2')
+  var base = require('level')('/tmp/test-sublevel2')
 
   var Sublevel = require('../')
 
@@ -48,8 +64,6 @@ test('sublevel - prefixed batches', function (t) {
 
   var a    = base.sublevel('A')
   var b    = base.sublevel('B')
-
-  var obj = {}
 
   base.batch([
     {key: 'a', value: 1, type: 'put'},
@@ -58,29 +72,24 @@ test('sublevel - prefixed batches', function (t) {
     {key: 'd', value: 4, type: 'put', prefix: a},
     {key: 'e', value: 5, type: 'put', prefix: base},
   ], function (err) {
-    base.createReadStream({end: '\xff\xff'})
-      .on('data', function (ch) {
-        obj[ch.key] = ch.value
+    all(base, function (_, obj) {
+      t.deepEqual(obj, {
+        'a': '1',
+        'c': '3',
+        'e': '5',
+        '~A~d': '4',
+        '~B~b': '2'
       })
-      .on('end', function () {
-        t.deepEqual(obj, {
-          'a': '1',
-          'c': '3',
-          'e': '5',
-          '~A~d': '4',
-          '~B~b': '2'
-        })
-        console.log(obj)
-        t.end()
-      })
+      console.log(obj)
+      t.end()
+    })
   })
-
 })
 
 test('sublevel - prefixed batches on subsection', function (t) {
 
   require('rimraf').sync('/tmp/test-sublevel3')
-  var base = require('levelup')('/tmp/test-sublevel3')
+  var base = require('level')('/tmp/test-sublevel3')
 
   var Sublevel = require('../')
 
@@ -89,8 +98,6 @@ test('sublevel - prefixed batches on subsection', function (t) {
   var a    = base.sublevel('A')
   var b    = base.sublevel('B')
 
-  var obj = {}
-
   a.batch([
     {key: 'a', value: 1, type: 'put', prefix: base},
     {key: 'b', value: 2, type: 'put', prefix: b},
@@ -98,21 +105,16 @@ test('sublevel - prefixed batches on subsection', function (t) {
     {key: 'd', value: 4, type: 'put'},
     {key: 'e', value: 5, type: 'put', prefix: base},
   ], function (err) {
-    base.createReadStream({end: '\xff\xff'})
-      .on('data', function (ch) {
-        obj[ch.key] = ch.value
+    all(base, function (_, obj) {
+      t.deepEqual(obj, {
+        'a': '1',
+        'c': '3',
+        'e': '5',
+        '~A~d': '4',
+        '~B~b': '2'
       })
-      .on('end', function () {
-        t.deepEqual(obj, {
-          'a': '1',
-          'c': '3',
-          'e': '5',
-          '~A~d': '4',
-          '~B~b': '2'
-        })
-        console.log(obj)
-        t.end()
-      })
+      t.end()
+    })
   })
 })
 
@@ -120,7 +122,7 @@ test('sublevel - prefixed batches on subsection', function (t) {
 test('sublevel - prefixed batches on subsection - strings', function (t) {
 
   require('rimraf').sync('/tmp/test-sublevel4')
-  var base = require('levelup')('/tmp/test-sublevel4')
+  var base = require('level')('/tmp/test-sublevel4')
 
   var Sublevel = require('../')
 
@@ -130,33 +132,26 @@ test('sublevel - prefixed batches on subsection - strings', function (t) {
   var b    = base.sublevel('B')
   var b_c    = b.sublevel('C')
 
-  var obj = {}
-
-  a.batch([
-    {key: 'a', value: 1, type: 'put', prefix: base.prefix()},
+  base.batch([
+    {key: 'a', value: 1, type: 'put'},
     {key: 'b', value: 2, type: 'put', prefix: b.prefix()},
-    {key: 'c', value: 3, type: 'put', prefix: base.prefix()},
-    {key: 'd', value: 4, type: 'put'},
-    {key: 'e', value: 5, type: 'put', prefix: base.prefix()},
+    {key: 'c', value: 3, type: 'put'},
+    {key: 'd', value: 4, type: 'put', prefix: a.prefix()},
+    {key: 'e', value: 5, type: 'put'},
     {key: 'f', value: 6, type: 'put', prefix: b_c.prefix()},
   ], function (err) {
-    base.createReadStream({end: '\xff\xff'})
-      .on('data', function (ch) {
-        obj[ch.key] = ch.value
+    all(base, function (_, obj) {
+      t.deepEqual(obj, {
+        'a': '1',
+        'c': '3',
+        'e': '5',
+        '~A~d': '4',
+        '~B~b': '2',
+        '~B~~C~f': '6'
       })
-      .on('end', function () {
-        console.log('D?', obj)
-        t.deepEqual(obj, {
-          'a': '1',
-          'c': '3',
-          'e': '5',
-          '~A~d': '4',
-          '~B~b': '2',
-          '~B~~C~f': '6'
-        })
-        console.log(obj)
-        t.end()
-      })
+      console.log(obj)
+      t.end()
+    })
   })
 })
 
