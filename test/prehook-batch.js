56c9774167ccdc0c990fedc381b06aa0f8ba5052
Dominic Tarr <dominic.tarr@gmail.com>
2013-08-13 11:36:07 +0100

    test that batch is passed to prehook on sublevels

--- test/prehook-batch.js
+++ test/prehook-batch.js
@@ -15,9 +15,42 @@ test('prehook can introspect whole batch', function (t) {
 
   var didHaveLog = 0, didNotHaveLog = 0
 
+  var prefix = logDb.prefix()
+
   db.pre(function (op, add, batch) {
     if(find(batch, function (_op) {
-      return op.key == _op.value && _op.prefix === logDb
+      return op.key == _op.value && _op.key.indexOf(prefix) === 0
+    }))
+      didHaveLog ++
+    else {
+      add({key: Date.now(), value: op.key, type: 'put', prefix: logDb})
+      didNotHaveLog ++
+    }
+  })
+
+  db.batch([
+    {key: 'foo', value: new Date(), type: 'put'},
+    {key: Date.now(), value: 'foo', type: 'put', prefix: logDb},
+  ], function (err) {
+    if(err) console.error(err.stack)
+    t.notOk(err, 'save did not error')
+    t.ok(didHaveLog)
+    t.end()
+  })
+
+})
+
+test('prehook can introspect whole batch - when sublevel', function (t) {
+
+  var db    = sublevel(level('introspect2')).sublevel('main')
+  var logDb = db.sublevel('log')
+
+  var didHaveLog = 0, didNotHaveLog = 0
+  var prefix = logDb.prefix()
+
+  db.pre(function (op, add, batch) {
+    if(find(batch, function (_op) {
+      return op.key == _op.value && _op.key.indexOf(prefix) === 0
     }))
       didHaveLog ++
     else {
