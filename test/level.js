bd22381569c4f86bc819859b7f8a4a27a79c6ac9
Dominic Tarr <dominic.tarr@gmail.com>
2014-02-03 06:22:44 +0700

    more tests

--- test/level.js
+++ test/level.js
@@ -6,32 +6,79 @@ var nut   = require('../nut')
 var shell = require('../shell') //the shell surrounds the nut
 var codec = require('key-order')
 
-var db = shell ( nut ( mock(), codec ) )
+function create () {
+  return shell ( nut ( mock(), codec ) )
+}
 
-tape('test - prehook', function (t) {
-  var db = shell ( nut ( mock(), codec ) )
+var db = create()
 
-  var log = db.sublevel('log')
-  var c = 0
-  db.pre(function (op, add) {
-    add({key: ''+c++, value: op.key, prefix: ['log']})
-  })
+function prehookPut (db) {
+  tape('test - prehook - put', function (t) {
+    var db = shell ( nut ( mock(), codec ) )
 
-  db.put('hello', 'there?', function (err) {
-    if(err) throw err
-    log.get('0', function (err, value) {
+    var log = db.sublevel('log')
+    var c = 0
+    db.pre(function (op, add) {
+      add({key: ''+c++, value: op.key, prefix: ['log']})
+    })
+
+    db.put('hello', 'there?', function (err) {
       if(err) throw err
-      t.equal(value, 'hello')
-      t.end()
+      log.get('0', function (err, value) {
+        if(err) throw err
+        t.equal(value, 'hello')
+        t.end()
+      })
     })
   })
-})
+}
 
-function posthook (args, calls) {
-  var db = shell ( nut ( mock(), codec ) )
+function prehookBatch (db) {
+  tape('test - prehook - put', function (t) {
+    var db = shell ( nut ( mock(), codec ) )
+
+    var log = db.sublevel('log')
+    var c = 0
+    db.pre(function (op, add) {
+      add({key: ''+c++, value: op.key, prefix: ['log']})
+    })
+
+    db.batch([
+      {key:'hello1', value: 'there.', type: 'put'},
+      {key:'hello2', value: 'there!', type: 'put'},
+      {key:'hello3', value: 'where?', type: 'put'},
+    ], function (err) {
+      if(err) throw err
+      log.get('0', function (err, value) {
+        if(err) throw err
+        t.equal(value, 'hello1')
+        log.get('1', function (err, value) {
+          if(err) throw err
+          t.equal(value, 'hello2')
+          log.get('2', function (err, value) {
+            if(err) throw err
+            t.equal(value, 'hello3')
+            t.end()
+          })
+        })
+      })
+    })
+  })
+}
+
+
+prehookPut(db)
+prehookPut(db.sublevel('foo'))
+
+var db2 = create()
+
+prehookBatch(db2)
+prehookBatch(db2.sublevel('foo'))
+//return
+function posthook (args, calls, db) {
+  db = db || shell ( nut ( mock(), codec ) )
 
   var method = args.shift()
-  calls = calls || 1
   tape('test - posthook - ' + method, function (t) {
 
     var cb = 0, hk = 0
@@ -57,12 +104,56 @@ function posthook (args, calls) {
   })
 }
 
-posthook(['put', 'hello', 'there?'])
-posthook(['del', 'hello', 'there?'])
-//posthook(['batch', [
-//  { key: 'foo', value: 'bar', type: 'put'},
-//  { key: 'fuz', value: 'baz', type: 'put'},
-//  { key: 'fum', value: 'boo', type: 'put'}
-//  ]], 3)
-//
+// test posthooks trigger correct number of times
+
+posthook(['put', 'hello', 'there?'], 1)
+posthook(['del', 'hello', 'there?'], 1)
+posthook(['batch', [
+  { key: 'foo', value: 'bar', type: 'put'},
+  { key: 'fuz', value: 'baz', type: 'put'},
+  { key: 'fum', value: 'boo', type: 'put'}
+  ]], 3)
+
+// test posthooks also work in sublevels
+
+posthook(['put', 'hello', 'there?'], 1, db.sublevel('a'))
+posthook(['del', 'hello', 'there?'], 1, db.sublevel('b'))
+posthook(['batch', [
+  { key: 'foo', value: 'bar', type: 'put'},
+  { key: 'fuz', value: 'baz', type: 'put'},
+  { key: 'fum', value: 'boo', type: 'put'}
+  ]], 3, db.sublevel('c'))
+
+//test removing hooks.
+
+function rmHook (db) {
+  tape('test - prehook - put', function (t) {
+    var db = shell ( nut ( mock(), codec ) )
+
+    var hk = 0
+    var rm = db.pre(function (op, add) {
+      hk ++
+      t.equal(op.key, 'hello')
+      t.equal(op.value, 'there')
+    })
+
+    db.put('hello', 'there', function (err) {
+      if(err) throw err
+      t.equal(hk, 1)
+      db.put('hello', 'where?', function (err) {
+        if(err) throw err
+        t.equal(hk, 1)
+        t.end()
+      })
+    })
+    rm()
+
+  })
+  
+}
+
+var db3 = create()
+
+rmHook(db3)
+rmHook(db3.sublevel('foo'))
 
