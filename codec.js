5edc9c6e58cb0d50790a8566453698cbdd20f5c5
Dominic Tarr <dominic.tarr@gmail.com>
2014-02-03 23:39:55 +1000

    import codec, since it's very small anyway

---
+++ codec.js
@@ -0,0 +1,14 @@
+
+//define the key ordering for level-sublevelq
+
+var join = '\x01', separate = '\x00'
+var join = '#', separate = '!'
+
+exports.encode = function (e) {
+  return separate + e[0].join(join) + separate + e[1]
+}
+
+exports.decode = function (s) {
+  var i = s.indexOf(separate, 1)
+  return [s.substring(1, i).split(join).filter(Boolean), s.substring(++i)]
+}
