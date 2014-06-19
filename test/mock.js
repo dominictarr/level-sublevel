6f1a3537771f9e5720a17b4be040b7e5b1a582b6
Dominic Tarr <dominic.tarr@gmail.com>
2014-06-19 03:46:10 +0200

    update mock to allow binary keys

--- test/mock.js
+++ test/mock.js
@@ -1,10 +1,29 @@
 var EventEmitter = require('events').EventEmitter
 var range = require('../range')
 var pull = require('pull-stream')
+var compare = require('../range').compare
 
 var next = 'undefined' === typeof setImmediate ? setTimeout : setImmediate
 
 var I = 0
+
+function insert (ary, op) {
+  for(var i in ary) {
+    var c = compare(ary[i].key, op.key)
+    if(c === 0)
+      return op.type === 'del' ? ary.splice(i, 1) : ary[i] = op
+    else if(c > 0)
+      return ary.splice(i, 0, op)
+  }
+  ary.push(op)
+}
+
+function get (ary, key) {
+  for(var i in ary)
+    if(compare(ary[i].key, key) === 0)
+      return ary[i].value
+  return null
+}
 
 module.exports = function () {
   if(process.env.FOR_REAL) {
@@ -14,14 +33,15 @@ module.exports = function () {
   }
 
   var emitter = new EventEmitter()
-  var data = emitter.data = {}
+  var data = emitter.data = []
 
   emitter.batch = function (ops, opts, cb) {
     ops.forEach(function (op) {
-      if(op.type === 'del')
-        delete data[op.key]
-      else
-        data[op.key] = op.value
+      insert(data, op)
+//      if(op.type === 'del')
+//        delete data[op.key]
+//      else
+//        data[op.key] = op.value
     })
     next(function () {
       emitter.emit('post', ops); cb()
@@ -29,7 +49,7 @@ module.exports = function () {
   }
 
   emitter.get = function (key, cb) {
-    var value = data[key]
+    var value = get(data, key)
     next(function () {
       if(!value) cb(new Error('404'))
       else       cb(null, value)
@@ -38,10 +58,10 @@ module.exports = function () {
 
   emitter.db = {}
   emitter.db.iterator = function (opts) {
-    var values = Object.keys(data).sort().filter(function (v) {
-      return range(opts, v)
-    }).map(function (key) {
-      return {key: key, value: data[key]}
+    var values = data.filter(function (v) {
+      return range(opts, v.key)
+    }).map(function (op) {
+      return {key: op.key, value: op.value}
     })
     if(opts.reverse) values.reverse()
 
