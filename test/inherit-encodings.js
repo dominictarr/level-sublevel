f77194555a8ad9494c9075a0b40072d39ad93288
Dominic Tarr <dominic.tarr@gmail.com>
2014-07-24 14:05:11 -0700

    test that options (such as encoding) is inherited by sublevels correctly.

---
+++ test/inherit-encodings.js
@@ -0,0 +1,55 @@
+
+
+var tape = require('tape')
+
+var sublevel = require('../')
+var level = require('level-test')()
+
+tape('inherit json encoding', function (t) {
+
+  var db = sublevel(level('simple', {encoding: 'json'}))
+  db.put('hello', {ok: true}, function (err) {
+    if(err) throw err
+
+    db.get('hello', function (err, value) {
+      if(err) throw err
+
+      t.deepEqual(value, {ok: true})
+      var db2 = db.sublevel('sub')
+      db2.put('hello', {ok: true}, function (err) { 
+        if(err) throw err
+
+        db2.get('hello', function (err, value) {
+          if(err) throw err
+          t.deepEqual(value, {ok: true})
+          t.end()
+        })
+      })
+    })
+  })
+})
+
+tape('override json encoding', function (t) {
+  var db = sublevel(level('level-sublevel_override', {encoding: 'json'}))
+  var buf = new Buffer([1,2,3,4])
+
+  db.put('hello', buf, function (err) {
+    if(err) throw err
+
+    db.get('hello', function (err, value) {
+      if(err) throw err
+
+      t.deepEqual(value, buf.toJSON())
+      var db2 = db.sublevel('sub', {valueEncoding: 'binary'})
+      db2.put('hello', buf, function (err) { 
+        if(err) throw err
+
+        db2.get('hello', function (err, value) {
+          if(err) throw err
+          t.deepEqual(value, buf)
+          t.end()
+        })
+      })
+    })
+  })
+})
