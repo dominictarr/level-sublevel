84f95f8c85c0f9865bae9a8a7e8c78fcba873894
Dominic Tarr <dominic.tarr@gmail.com>
2014-07-29 18:39:17 -0700

    support some useful encodings

---
+++ codec/bytewise.js
@@ -0,0 +1,8 @@
+var bytewise = require('bytewise')
+
+module.exports = {
+  encode: bytewise.encode,
+  decode: bytewise.decode,
+  lowerBound: undefined,
+  upperBound: null
+}
---
+++ legacy.js
@@ -0,0 +1,16 @@
+var nut   = require('./nut')
+var shell = require('./shell') //the shell surrounds the nut
+var codec = require('levelup/lib/codec')
+// Currently this uses pull streams,
+// and not levelup's readstream, but in theory
+// I should be able pretty much just drop that in.
+var ReadStream = require('levelup/lib/read-stream')
+
+var precodec = require('./codec/legacy')
+
+module.exports = function (db) {
+
+  return shell ( nut ( db, precodec, codec ), [], ReadStream, db.options)
+
+}
+
